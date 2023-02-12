c1efb6c930c03590f73e985570b092fcb1660a05
ed-hipolito <git>
2023-02-12 22:21:15 +0800

    Initialize schedule push notification

--- App.js
+++ App.js
@@ -1,36 +1,93 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+import React, { useEffect, useState, useRef } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { decode, encode } from 'base-64';
+
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens';
+
+if (!global.btoa) {
+	global.btoa = encode;
+}
+if (!global.atob) {
+	global.atob = decode;
+}
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false
+	})
+});
 
 const Stack = createStackNavigator();
 
 export default function App() {
+	const [loading, setLoading] = useState(true);
+	const [user, setUser] = useState(null);
 
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+	useEffect(() => {
+		registerForPushNotificationsAsync()
+			.then((x) => {
+				console.log(x);
+			})
+			.catch((x) => {
+				console.error(x);
+			});
+	}, []);
 
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        { user ? (
-          <Stack.Screen name="Home" options={{ headerShown: false }}>
-            {props => <HomeScreen {...props} extraData={user} />}
-          </Stack.Screen>
-        ) : (
-          <>
-            <Stack.Screen name="Login">
-              {props => <LoginScreen {...props} setUser={setUser} />}
-            </Stack.Screen>
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				{user ? (
+					<Stack.Screen name='Home' options={{ headerShown: false }}>
+						{(props) => <HomeScreen {...props} extraData={user} />}
+					</Stack.Screen>
+				) : (
+					<>
+						<Stack.Screen name='Login'>{(props) => <LoginScreen {...props} setUser={setUser} />}</Stack.Screen>
+						<Stack.Screen name='Registration' component={RegistrationScreen} />
+					</>
+				)}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
+
+const registerForPushNotificationsAsync = async () => {
+	let token;
+
+	if (Platform.OS === 'android') {
+		await Notifications.setNotificationChannelAsync('default', {
+			name: 'default',
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: '#FF231F7C'
+		});
+	}
+
+	if (Device.isDevice) {
+		const { status: existingStatus } = await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+
+		if (existingStatus !== 'granted') {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+
+		if (finalStatus !== 'granted') {
+			alert('Failed to get push token for push notification!');
+			return;
+		}
+
+		token = (await Notifications.getExpoPushTokenAsync()).data;
+		console.log(token);
+	} else {
+		alert('Must use physical device for Push Notifications');
+	}
+
+	return token;
+};
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -3,103 +3,163 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import YoutubeCard from './CardVideo';
 import ProfileScreen from '../ProfileScreen/ProfileScree';
 
+// imports for Schedule Screen
+import { useState } from 'react';
+import * as Notifications from 'expo-notifications';
+
 function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <YoutubeCard/>
-    </View>
-  );
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<YoutubeCard />
+		</View>
+	);
 }
 
 function ScheduleScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Schedule!</Text>
- </View>
-  );
+	const [notificationData, setNotificationData] = useState({
+		content: {
+			title: 'Robert the gayman',
+			body: `Here is the notification body.`,
+			data: { data: 'goes here' }
+		},
+		trigger: {
+			// weekday: 0,
+			// hour: 0,
+			// minute: 0,
+			seconds: 10,
+			repeats: true
+		}
+	});
+
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Button
+				title='Press to schedule a notification'
+				onPress={async () => {
+					await createLocalPushNotification(notificationData);
+				}}
+			/>
+			<Button
+				title='Remove all scheduled notifications.'
+				onPress={async () => {
+					console.log('Removed All Scheduled Notifications.');
+					await Notifications.cancelAllScheduledNotificationsAsync();
+				}}
+			/>
+			<Button
+				title='Get all scheduled notifications.'
+				onPress={async () => {
+					const scheduledNotifs = await Notifications.getAllScheduledNotificationsAsync();
+					console.log(`Scheduled Notifs Length: ${scheduledNotifs.length}`);
+					scheduledNotifs.forEach((x) => {
+						console.log(x);
+					});
+				}}
+			/>
+		</View>
+	);
 }
 
+const createLocalPushNotification = async (schedule) => {
+	await Notifications.scheduleNotificationAsync(schedule);
+};
+
 function Search() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-       <Text>Search!</Text>
-    </View>
-  );
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Search!</Text>
+		</View>
+	);
 }
 
 const Profile = () => {
-  return (
-    <ProfileScreen/>
-  );
+	return <ProfileScreen />;
 };
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-  return (
-    <Tab.Navigator>
+	return (
+		<Tab.Navigator>
+			<Tab.Screen
+				name='HomeTab'
+				component={HomeScreen}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<View style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}>
+							<Image
+								source={require('../../../assets/cottage.png')}
+								resizeMode='contain'
+								style={{
+									width: 25,
+									height: 25
+								}}
+							/>
+						</View>
+					)
+				}}
+			/>
 
-      <Tab.Screen name="HomeTab" component={HomeScreen} options={{
-        tabBarIcon: ({focused}) => (
-          <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-            <Image 
-              source={require('../../../assets/cottage.png')}
-              resizeMode='contain'
-              style={{
-              width: 25,
-              height: 25,}}
-              />
-          </View>
-        ),
-      }}/>
+			<Tab.Screen
+				name='Schedule'
+				component={ScheduleScreen}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<View style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}>
+							<Image
+								source={require('../../../assets/import_contacts.png')}
+								resizeMode='contain'
+								style={{
+									width: 25,
+									height: 25
+								}}
+							/>
+						</View>
+					)
+				}}
+			/>
 
-      <Tab.Screen name="Schedule" component={ScheduleScreen} options={{
-        tabBarIcon: ({focused}) => (
-          <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-            <Image 
-              source={require('../../../assets/import_contacts.png')}
-              resizeMode='contain'
-              style={{
-              width: 25,
-              height: 25,}}
-              />
-          </View>
-        ),
-      }}/>
+			<Tab.Screen
+				name='Search'
+				component={Search}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<View style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}>
+							<Image
+								source={require('../../../assets/search.png')}
+								resizeMode='contain'
+								style={{
+									width: 25,
+									height: 25
+								}}
+							/>
+						</View>
+					)
+				}}
+			/>
 
-      <Tab.Screen name="Search" component={Search} options={{
-        tabBarIcon: ({focused}) => (
-          <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-            <Image 
-              source={require('../../../assets/search.png')}
-              resizeMode='contain'
-              style={{
-              width: 25,
-              height: 25,}}
-              />
-          </View>
-        ),
-      }}/>
-
-      <Tab.Screen name="Profile" component={Profile} options={{
-        tabBarIcon: ({focused}) => (
-          <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-            <Image 
-              source={require('../../../assets/account_circle.png')}
-              resizeMode='contain'
-              style={{
-              width: 25,
-              height: 25,}}
-              />
-          </View>
-        ),
-      }}/>
-    </Tab.Navigator>
-  );
+			<Tab.Screen
+				name='Profile'
+				component={Profile}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<View style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}>
+							<Image
+								source={require('../../../assets/account_circle.png')}
+								resizeMode='contain'
+								style={{
+									width: 25,
+									height: 25
+								}}
+							/>
+						</View>
+					)
+				}}
+			/>
+		</Tab.Navigator>
+	);
 }
 
 export default function NavBottom() {
-  return (
-      <MyTabs />
-  );
+	return <MyTabs />;
 }
