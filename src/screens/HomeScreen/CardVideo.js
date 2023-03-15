e5d6d3dae2be4f20c3b4ad7b32d24f826ff04663
Myst <cedrick89x@gmail.com>
2023-03-15 11:41:19 +0800

    remove unnecessary files/ lines of codes

--- src/screens/HomeScreen/CardVideo.js
+++ src/screens/HomeScreen/CardVideo.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity, ScrollView } from "react-native";
 
-import Row1 from "./VideoComponent/row1";
-import Row2 from "./VideoComponent/row2";
 import VideoCarousel from "./VideoComponent/VideoPlayer";
-
-const videoIds = ["npHIpM1BE8k", "oth9-0gkYnw", "UeGdCxH_oHY", "Fk8-XldewAk"];
 
 export default function YoutubeCard() {
 
