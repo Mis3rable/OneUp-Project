e01b0ca8596abf14f7b4960b107deb6d14efa076
Myst <cedrick89x@gmail.com>
2023-03-31 12:01:42 +0800

    added LogBox.ignoreLogs

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -7,6 +7,11 @@ import React, {useEffect} from 'react'
 import YoutubeCard from './Home';
 import Constants from 'expo-constants';
 const Tab = createBottomTabNavigator();
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state',
+]);
 
 export default function MyTabs({ navigation, user, setUser }) {
   const CustomHeader = ({ navigation, route }) => {
